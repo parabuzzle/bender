ff23bf0625c42101991ccb089e756c9a1e9ec570
Mike Heijmans <parabuzzle@gmail.com>
2015-02-20 14:13:16 -0800

    Heroku-ify the code
      * Moved all configuration to ENV variables
      * Setup Procfile for foreman/heroku

---
+++ Gemfile
@@ -0,0 +1,5 @@
+source "https://rubygems.org"
+
+gem 'spunk', '~> 0.1.3'
+gem 'bundler'
+gem 'pry'
--- lib/http_listener.rb
+++ lib/http_listener.rb
@@ -3,21 +3,22 @@ require 'webrick'
 module HttpListener
 
   def HttpListener.start_http_listener(bot)
-    @bot = bot
-    p = CONFIG['http_listener']
-    if p['log'] == "STDOUT"
-      log = STDOUT
-    elsif p['log'] == "!BENDER_LOGGER"
-      log = Log
-    else
-      log = p['log']
+    @bot           = bot
+    log_file       = ENV['HTTP_LOG_FILE']       || STDOUT
+    port           = ENV['PORT']                || 9091
+    max_clients    = ENV['HTTP_MAX_CLIENTS']    || 4
+    listen_address = ENV['HTTP_LISTEN_ADDRESS'] || '0.0.0.0'
+
+    if log_file == "!BENDER_LOGGER"
+      log_file = Log
     end
+
     begin
-      Log.info("starting http listener on port #{p['port']}...")
-      server = WEBrick::HTTPServer.new(:Port => p['port'], 
-                                      :MaxClients => p['max_clients'], 
-                                      :Logger => WEBrick::Log.new(log), 
-                                      :BindAddress => p['address'],
+      Log.info("starting http listener on port #{port}...")
+      server = WEBrick::HTTPServer.new(:Port => port,
+                                      :MaxClients => max_clients,
+                                      :Logger => WEBrick::Log.new(log_file),
+                                      :BindAddress => listen_address,
                                       :bot=>bot)
       Log.info "Starting servlets"
       # load servlets
@@ -26,8 +27,8 @@ module HttpListener
         klass = file.camelize.classify
         server.mount klass.class_eval{@mountpoint}, klass
         Log.info "Starting servlet #{file.camelize} mounted at #{klass.class_eval{@mountpoint}}"
-      end 
-        
+      end
+
       trap "INT" do server.shutdown end
       server.start
     ensure

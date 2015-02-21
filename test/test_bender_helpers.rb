dd945665c32b874195ab33a5878cc74a7f6a01d2
Mike Heijmans <parabuzzle@gmail.com>
2015-02-20 17:19:12 -0800

    fix tests

--- test/test_bender_helpers.rb
+++ test/test_bender_helpers.rb
@@ -10,23 +10,22 @@ require 'test/unit'
 require "#{@libdir}/lib/bender_helpers.rb"
 include BenderHelpers
 
-CONFIG = YAML::load(File.open("#{@libdir}/config.yml")) unless defined?(CONFIG)
-CONFIG['pid_file'] += ".test" # Need to change the pid file name for test purposes..
 Log = Logger.new("/dev/null") unless defined?(Log)
+ENV['PID_FILE'] = './bender.pid.test'
 
 class TestBenderHelpers < Test::Unit::TestCase
-  
+
   def setup
-    @pid_file = CONFIG['pid_file']
+    @pid_file = ENV['PID_FILE']
     if File.exist?(@pid_file)
       File.delete(@pid_file)
     end
     @mypid = bender_pid
   end
-  
+
   def teardown
   end
-  
+
   def test_pid_handling
     assert !File.exist?(@pid_file)        # check that there is no pid file
     assert !pid_running?(123456)          # check pid_running? returns false on a bogus pid
@@ -36,5 +35,5 @@ class TestBenderHelpers < Test::Unit::TestCase
     assert remove_pid                     # remove pid file
     assert !File.exist?(@pid_file)        # check that pid file was removed
   end
-  
+
 end
--- test/test_string-mixins.rb
+++ test/test_string-mixins.rb
@@ -12,29 +12,29 @@ class AbcDef
 end
 
 class TestStringMixins < Test::Unit::TestCase
-  
+
   def setup
     @str = "abc_def"
   end
-  
+
   def teardown
   end
-  
+
   def test_capitalize
     assert_not_equal @str, "Abc_def"                                          # check that string is not capitalized
     assert_equal @str.capitalize_first_letter, "Abc_def"                      # check that string is now capitalized
     assert_not_equal @str.capitalize_first_letter.object_id, @str.object_id   # check that string didn't capitalize in place
   end
-  
+
   def test_camelize
     assert_not_equal @str, "AbcDef"                   # check that string is not camel cased
     assert_equal @str.camelize, "AbcDef"              # check that camelize properly camel cased the string
     assert_equal "abc_def_ghi".camelize, "AbcDefGhi"  # check that multi set camel casing works
   end
-  
+
   def test_classify
     assert_not_equal @str.class, AbcDef.class                 # Test that string is not of AbcDef class
     assert_equal @str.camelize.classify.class, AbcDef.class   # Test that Classify properly returns a AbcDef object
   end
-  
+
 end

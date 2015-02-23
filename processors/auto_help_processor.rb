58cdf123a698d59a23d60039b81776b5060d9742
Mike Heijmans <parabuzzle@gmail.com>
2015-02-22 20:24:39 -0800

    Added help processor

---
+++ processors/auto_help_processor.rb
@@ -0,0 +1,16 @@
+module Bender::Processor
+  class AutoHelpProcessor < Bender::BaseProcessor
+    def process
+      respond(/help$/i) do
+        help_messages.each { |msg| reply msg }
+      end
+    end
+
+    def help_messages
+      Bender::Processor.constants.each_with_object([]) do |const, ary|
+        klass = "Bender::Processor::#{const.to_s}".classify
+        ary << klass.help if klass.respond_to?(:help)
+      end.flatten
+    end
+  end
+end
--- processors/basic_processor.rb
+++ processors/basic_processor.rb
@@ -5,5 +5,13 @@ module Bender::Processor
       respond(/PING$/) { reply "PONG" }
       hear(/^#{@bot.nickname}\??$/) { reply 'Kiss my shiny metal ass' }
     end
+
+    def self.help
+      [
+        "#{Bender.nickname} version - replies with the Bender Version",
+        "#{Bender.nickname} PING - replies with 'PONG'",
+        "#{Bender.nickname}? - replies with 'Kiss my shiny metal ass"
+      ]
+    end
   end
 end
